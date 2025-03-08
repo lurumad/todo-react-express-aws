a94625d98cd3a17561035dab26e91519ce392ae7
lurumad <luisruizpavon@gmail.com>
2025-03-08 09:04:58 +0100

    test: change router setup

--- backend/src/setup/router.test.ts
+++ backend/src/setup/router.test.ts
@@ -1,12 +1,13 @@
 import express from "express";
 import request from "supertest";
 import { routerContollersSetup } from "./router.controllers";
+import { routerSetup } from "./router";
 
 describe("routerSetup", async () => {
   it("returns pong", async () => {
     const app = express();
 
-    routerContollersSetup(app);
+    routerSetup(app);
 
     await request(app)
       .get("/ping")
